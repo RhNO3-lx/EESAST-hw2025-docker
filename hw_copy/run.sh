ed2cd243cddc22f64ba23afae8b1a91e9ac53091
RhNO3-lx <3196486451@qq.com>
2025-09-02 20:21:39 +0800

    肖裕杰_2023011346_笃实44

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,8 @@
-# Add your commands here
+#!/bin/bash
+
+cd ..
+docker build -t hw_copy -f hw_copy/Dockerfile .
+
+docker run hw_copy /app/add 3 5
+
+docker run hw_copy /app/mul 3 5
--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
 # Add your commands here
+cd ..
+docker build -t hw_vol -f hw_vol/Dockerfile .
+
+docker run -v $(pwd)/src:/app hw_vol sh -c "g++ -o /app/add /app/add.cpp && /app/add 3 5"
+
+docker run -v $(pwd)/src:/app hw_vol sh -c "g++ -o /app/mul /app/mul.cpp && /app/mul 3 5"
